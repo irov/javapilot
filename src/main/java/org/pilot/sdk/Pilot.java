6f3c06d64190b8adaa65107ada50cea079deaa5b
yurii.levchenko <yurii.levchenko@murka.com>
2026-03-26 18:43:33 +0200

    bump 1.0.13

--- src/main/java/org/pilot/sdk/Pilot.java
+++ src/main/java/org/pilot/sdk/Pilot.java
@@ -65,7 +65,7 @@ import java.util.concurrent.atomic.AtomicReference;
  * }</pre>
  */
 public final class Pilot {
-    public static final String VERSION = "1.0.12";
+    public static final String VERSION = "1.0.13";
 
     private static volatile Pilot s_instance;
 
