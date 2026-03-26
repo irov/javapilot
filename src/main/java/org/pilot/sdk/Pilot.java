dea364bf60e973559ad08e8eed5bec6ab3883fd8
yurii.levchenko <yurii.levchenko@murka.com>
2026-03-26 17:45:20 +0200

    feat: add Pilot.VERSION constant

--- src/main/java/org/pilot/sdk/Pilot.java
+++ src/main/java/org/pilot/sdk/Pilot.java
@@ -65,6 +65,8 @@ import java.util.concurrent.atomic.AtomicReference;
  * }</pre>
  */
 public final class Pilot {
+    public static final String VERSION = "1.0.11";
+
     private static volatile Pilot s_instance;
 
     private final PilotConfig m_config;
